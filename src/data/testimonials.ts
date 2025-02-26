b9f1bac8e4dc00cca846f3db281c63355f0b60d8
victory balogun <victorybalogun@gmail.com>
2025-02-26 14:48:24 +0100

    Updated testimonials

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -1,20 +1,20 @@
 export const testimonialData = [
   {
-    content: "Victory is a talented developer with a great work ethic. His ability to solve complex problems and deliver scalable solutions is impressive. I have no doubt about his future in tech.",
+    content: "Victory is a talented developer with a great work ethic. Her ability to solve complex problems and deliver scalable solutions is impressive. I have no doubt about her future in tech.",
     author: "Israel Oyinlola",
     role: "Senior Software Engineer & Founder",
     company: "African AI Foundation",
     image: "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&auto=format&fit=crop&w=256&q=80"
   },
   {
-    content: "Victory is a critical part of our team, always bringing innovative ideas and a collaborative spirit. His dedication and technical expertise have been pivotal to our success.",
+    content: "Victory is a critical part of our team, always bringing innovative ideas and a collaborative spirit. Her dedication and technical expertise have been pivotal to our success.",
     author: "Vasco Goncalves",
     role: "Chief Information Officer",
     company: "Seven-Up Bottling Company",
     image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&auto=format&fit=crop&w=256&q=80"
   },
   {
-    content: "Working with Victory has been a pleasure. His frontend skills, especially in React and TypeScript, are top-notch, and he always strives for clean, maintainable code.",
+    content: "Working with Victory has been a pleasure. Her frontend skills, especially in React and TypeScript, are top-notch, and she always strives for clean, maintainable code.",
     author: "Happiness Balogun",
     role: "Senior Frontend Developer",
     company: "OptiSource Technologies",
